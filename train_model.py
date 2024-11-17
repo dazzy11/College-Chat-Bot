4154a55256ebdc71c870c9948bf47a0c01c02f07
Deepak madhukumar <deepakmadhukumar1124@gmail.com>
2024-11-17 10:36:07 +0530

    Added UI

--- train_model.py
+++ train_model.py
@@ -2,6 +2,8 @@ import json
 import pickle
 import numpy as np
 import nltk
+nltk.download('punkt')
+
 from nltk.stem import WordNetLemmatizer
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout
